8721619c5cc46fe57a44ebeee72c1c8e230b908e
Kimcheeee <102534231+Kimcheeee@users.noreply.github.com>
2024-10-08 21:42:37 +0100

    done

--- app.py
+++ app.py
@@ -7,17 +7,19 @@ db = Database('database.db')
 
 @app.route('/')
 def list_ews():
-    """
-    TO IMPLEMENT
-    """
-    return 'Get coding!'
+    rows = db.get_ews()
+    return render_template('list.html', ews=rows)
 
 @app.route('/add', methods=['POST'])
 def add_ew():
-    """
-    TO IMPLEMENT
-    """
-    pass
+    task = request.form['task']
+    subject = request.form['subject']
+    beak = request.form['beak']
+    dueDate = request.form['dueDate']
+
+    db.create_ew(task, subject, beak, dueDate)
+
+    return redirect('/')
 
 
 # EXTRA CREDIT
--- db.py
+++ db.py
@@ -2,37 +2,51 @@ import sqlite3
 
 class Database:
     def __init__(self, url):
+        self.url = url
+
         with sqlite3.connect(url) as db:
             cursor = db.cursor()
             sql = """
             CREATE TABLE IF NOT EXISTS ew (
                 id INTEGER PRIMARY KEY,
-                task TEXT NOT NULL
+                task TEXT NOT NULL,
+                subject TEXT NOT NULL,
+                beak TEXT NOT NULL,
+                dueDate TEXT NOT NULL
             );
             """
             cursor.execute(sql)
 
             sql_insert = """
-            INSERT INTO ew (task) VALUES (?);
+            INSERT INTO ew (task, subject, beak, dueDate) VALUES (?, ?, ?, ?);
             """
-            cursor.execute(sql_insert, ('Complete Flask web dev',))
+            cursor.execute(sql_insert, ('Task', 'Subject', 'Beak', '28/02/2007',))
             db.commit()
 
 
     def get_ews(self):
-        """
-        TO IMPLEMENT
-        """
-        pass
+        with sqlite3.connect(self.url) as db:
+            cursor = db.cursor()
 
-    def create_ew(self, task):
-        """
-        TO IMPLEMENT
-        """
-        pass
+            sql_get = """
+            SELECT id, task, subject, beak, dueDate FROM ew;
+            """
+            cursor.execute(sql_get)
+            rows = cursor.fetchall()
+            db.commit()
+            return rows
 
 
+    def create_ew(self, task, subject, beak, dueDate):
+        with sqlite3.connect(self.url) as db:
+            cursor = db.cursor()
 
+            sql_insert = """
+            INSERT INTO ew (task, subject, beak, dueDate) VALUES (?, ?, ?, ?);
+            """
+            cursor.execute(sql_insert, (task, subject, beak, dueDate))
+            db.commit()
+        
     # EXTRA CREDIT
     def get_ew(self, id):
         """
